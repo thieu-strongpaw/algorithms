b1918df51da91bf7744a8a3c96a7bbbb3ac76437
Matt Garcia <strongpawcoding@gmail.com>
2024-01-21 09:21:40 -0600

    Commiting the change to the new location

--- Insertion_Sort/Insertion_Sort.c
+++ Insertion_Sort/Insertion_Sort.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #define array_size 10
 
-void insertion_sort(int *array);
+void insertion_sort(int *array, int arr_size);
 
 int main(){
 	int int_array[array_size] = {3,4,67,44,2,7,4,54,23,1};
 
-	insertion_sort(int_array);
+	insertion_sort(int_array, array_size);
 
 	for(size_t i = 0; i < array_size; ++i){
-		printf("%d element is %d\n", i, int_array[i]);
+		printf("%ld element is %d\n", i, int_array[i]);
 	}
 }
 
-void insertion_sort(int *array){
+void insertion_sort(int *array, int arr_size){
 	size_t i, j, key;
-	for(j = 1; j < array_size; ++j){
+	for(j = 1; j < arr_size; ++j){
 		key = array[j];
 		i = j - 1;
 		while(i >= 0 && array[i] > key){
